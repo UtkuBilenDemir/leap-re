83b5210a7f103080dc577d7627dda5d008c7b02e
ubd <ubd@YogaFlame.YogaTeleport>
2021-06-09 17:11:14 +0200

    After-meeting adjustments

--- 03_analysis/02.01_map.R
+++ 03_analysis/02.01_map.R
@@ -3,244 +3,23 @@ library(bannerCommenter)
 library(stringr)
 library(htmlwidgets)
 library(htmltools)
+library(leaflet)
 M_05 <- readRDS(file = "01_data/02_bibliometrix/05_dataframe_seperated-aff_-_cou_cleaned.Rds")
 unsd_regions <- readRDS(file = "01_data/02_bibliometrix/unsd_regions_dict.Rds")
+unsd_regions$carto_names
+unsd_regions$au_regions <- unsd_regions$unsd_region
 
+unsd_regions$au_regions[unsd_regions$au_regions == "Middle Africa"] <- "Central Africa"
+unsd_regions$au_regions[10] <-  "Eastern Africa"
+unsd_regions$au_regions[12] <-  "Southern Africa"
+unsd_regions$au_regions[13] <-  "Southern Africa"
+unsd_regions$au_regions[21] <-  "Southern Africa"
+unsd_regions$au_regions[22] <-  "Southern Africa"
+unsd_regions$au_regions[26] <-  "Southern Africa"
+unsd_regions$au_regions[32] <-  "Central Africa"
+unsd_regions$au_regions[41] <-  "Northern Africa"
 
-
-
-## # ## Grab African topojson
-## 
-## # Topo correct names
-## unsd_regions$topo_names <- str_to_title(rownames(unsd_regions))
-## 
-## # Which ones are not present in the geojson data
-## not_ind <- which(!(unsd_regions$topo_names %in% unique(africa$geounit)))
-## unsd_regions$topo_names[not_ind]
-## unsd_regions$topo_names[not_ind[1]] <- unique(africa$geounit)[9]
-## unsd_regions$topo_names[not_ind[2]] <- unique(africa$geounit)[7]
-## unsd_regions$topo_names[not_ind[3]] <- unique(africa$geounit)[22]
-## unsd_regions$topo_names[not_ind[4]] <- unique(africa$geounit)[10]
-## unsd_regions$topo_names[not_ind[5]] <- unique(africa$geounit)[6]
-## # Mauritius and Eswatini are problematic
-## unsd_regions$topo_names[not_ind[12]] <- unique(africa$geounit)[48]
-## 
-## ## Eliminate the unmatched ones
-## unmatched_countries <- unsd_regions$topo_names[which(!(unsd_regions$topo_names %in% unique(africa$geounit)))]
-## 
-## 
-## M_05$Country_names <- unsd_regions[M_05$Country,2]
-## 
-## pub_freq <- as.data.frame(table(M_05$Country_names) )
-## colnames(pub_freq) <- c("Countries", "# of pub.")
-## 
-## pub_freq$`# of pub.`
-## rownames(pub_freq) <- pub_freq$Countries
-## 
-## pub_freq <- pub_freq$`# of pub.`
-## 
-## pubf <- as.data.frame(pub_freq[,2])
-## rownames(pubf) <- rownames(pub_freq)
-## colnames(pubf) <- "freq"
-## 
-## q <- pubf[-c(which(rownames(pubf) %in% unmatched_countries)),]
-## p <- rownames(pubf)[-c(which(rownames(pubf) %in% unmatched_countries))]
-## 
-## pf <- as.data.frame(q)
-## rownames(pf) <- p
-## colnames(pf) <- "Freq."
-## 
-## canvasXpress(
-##   data=pfn,
-##   # varAnnot=rownames(pubf),
-##   colorBy="freq",
-##   graphType="Map",
-##   # legendOrder=list(Winner=list("Republican", "Democrat")),
-##   mapId="freq",
-##   # mapProjection="albers",
-##   theme="wallStreetJournal",
-##   title="2000 Presidential Elections",
-##   topoJSON="https://www.canvasxpress.org/data/africa.geo.json"
-## )
-## 
-## africa$geounit[41] <- "Spania"
-## africa$geounit[15] <- "Spaniar"
-## pfn <- as.data.frame(pf[africa$geounit,])
-## rownames(pfn) <- africa$geounit
-## colnames(pfn) <- "freq"
-## 
-## table(africa$geounit)
-## 
-## str(afr)
-## 
-## 
-## 
-## 
-## y=read.table("https://www.canvasxpress.org/data/cX-CO2T-dat.txt", header=TRUE, sep="\t", quote="", row.names=1, fill=TRUE, check.names=FALSE, stringsAsFactors=FALSE)
-## z=read.table("https://www.canvasxpress.org/data/cX-CO2T-var.txt", header=TRUE, sep="\t", quote="", row.names=1, fill=TRUE, check.names=FALSE, stringsAsFactors=FALSE)
-## 
-## rownames(pfn)
-## 
-## world_countries <- as.data.frame(rownames(z))
-## rownames(world_countries) <- z$Country
-## 
-## pfnm <- pfn 
-## rownames(pfnm) <- world_countries[rownames(pfn),]
-## 
-## rownames(pfn)[7]
-## 
-## 
-## 
-## 
-## 
-## 
-## 
-## 
-## # Palette of 30 colors
-## library(RColorBrewer)
-## my_colors <- brewer.pal(9, "Reds") 
-## my_colors <- colorRampPalette(my_colors)(30)
-## 
-## # Attribute the appropriate color to each country
-## class_of_country <- cut(africa@data$POP2005, 30)
-## my_colors <- my_colors[as.numeric(class_of_country)]
-## 
-## # Make the plot
-## plot(africa , xlim=c(-20,60) , ylim=c(-40,40), col=my_colors ,  bg = "#A6CAE0")
-## 
-## 
-## 
-## 
-## 
-## library(rgdal)
-## my_spdf <- readOGR(dsn=path.expand("C:/Users/Downloads/Files/World-Shapefiles"), layer="TM_WORLD_BORDERS_SIMPL-0.3")
-## 
-## 
-## 
-## # Keep only data concerning Africa
-## africa <- my_spdf[my_spdf@data$REGION==2 , ]
-## 
-## # Plot africa
-## par(mar=c(0,0,0,0))
-## plot(africa , xlim=c(-20,60) , ylim=c(-40,35), col="steelblue", lwd=0.5 )
-## 
-## 
-## 
-## 
-## 
-## 
-## 
-## 
-## 
-## 
-## 
-## 
-## # Download the shapefile. (note that I store it in a folder called DATA. You have to change that if needed.)
-## download.file("http://thematicmapping.org/downloads/TM_WORLD_BORDERS_SIMPL-0.3.zip" , destfile="DATA/world_shape_file.zip")
-## # You now have it in your current working directory, have a look!
-## 
-## # Unzip this file. You can do it with R (as below), or clicking on the object you downloaded.
-## system("unzip 01_data/99_supplementary/TM_WORLD_BORDERS_SIMPL-0.3.zip")
-## #  -- > You now have 4 files. One of these files is a .shp file! (TM_WORLD_BORDERS_SIMPL-0.3.shp)
-## 
-## 
-## 
-## # Read this shape file with the rgdal library. 
-## library(rgdal)
-## world_spdf <- readOGR( 
-##   dsn= paste0(getwd(),"/") , 
-##   layer="TM_WORLD_BORDERS_SIMPL-0.3",
-##   verbose=FALSE
-## )
-## 
-## # Clean the data object
-## library(dplyr)
-## world_spdf@data$POP2005[ which(world_spdf@data$POP2005 == 0)] = NA
-## world_spdf@data$POP2005 <- as.numeric(as.character(world_spdf@data$POP2005)) / 1000000 %>% round(2)
-## 
-## # -- > Now you have a Spdf object (spatial polygon data frame). You can start doing maps!
-## 
-## # Create a color palette with handmade bins.
-## library(RColorBrewer)
-## library(leaflet)
-## mybins <- c(0,10,20,50,100,500,Inf)
-## mypalette <- colorBin( palette="YlOrBr", domain=world_spdf@data$POP2005, na.color="transparent", bins=mybins)
-## 
-## 
-## 
-## # Prepare the text for tooltips:
-## mytext <- paste(
-##   "Country: ", world_spdf@data$NAME,"<br/>", 
-##   "Area: ", world_spdf@data$AREA, "<br/>", 
-##   "Population: ", round(world_spdf@data$POP2005, 2), 
-##   sep="") %>%
-##   lapply(htmltools::HTML)
-## 
-## # Final Map
-## leaflet(world_spdf) %>% 
-##   addTiles()  %>% 
-##   setView( lat=10, lng=0 , zoom=2) %>%
-##   addPolygons( 
-##     fillColor = ~mypalette(POP2005), 
-##     stroke=TRUE, 
-##     fillOpacity = 0.9, 
-##     color="white", 
-##     weight=0.3,
-##     label = mytext,
-##     labelOptions = labelOptions( 
-##       style = list("font-weight" = "normal", padding = "3px 8px"), 
-##       textsize = "13px", 
-##       direction = "auto"
-##     )
-##   ) %>%
-##   addLegend( pal=mypalette, values=~POP2005, opacity=0.9, title = "Population (M)", position = "bottomleft" )
-## 
-## m  
-## 
-## # save the widget in a html file if needed.
-## # library(htmlwidgets)
-## # saveWidget(m, file=paste0( getwd(), "/HtmlWidget/choroplethLeaflet5.html"))
-## 
-## 
-## 
-## 
-## # Palette of 30 colors
-## library(RColorBrewer)
-## my_colors <- brewer.pal(9, "Reds") 
-## my_colors <- colorRampPalette(my_colors)(30)
-## 
-## # Attribute the appropriate color to each country
-## class_of_country <- cut(africa@data$POP2005, 30)
-## my_colors <- my_colors[as.numeric(class_of_country)]
-## 
-## # Make the plot
-## plot(africa , xlim=c(-20,60) , ylim=c(-40,40), col=my_colors ,  bg = "#A6CAE0")
-## 
-## 
-## 
-## 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 ## -------------------------------------------------------------------------------
 
 
@@ -313,7 +92,8 @@ M_05$Country_names <- unsd_regions[M_05$Country,2]
 au_freq <- as.data.frame(as.data.frame(table(M_05$Country_names))$Freq)
 rownames(au_freq) <- as.data.frame(table(M_05$Country_names))$Var1
 
-au_regions <- as.data.frame(unsd_regions$unsd_region[-c(56)])
+### au_regions <- as.data.frame(unsd_regions$unsd_region[-c(56)])
+au_regions <- as.data.frame(unsd_regions$au_region[-c(56)])
 rownames(au_regions) <- unsd_regions$carto_names[-c(56)]
 nrow(unsd_regions[56,])
 
@@ -392,7 +172,8 @@ saveRDS(map_pub_freq, file = "01_data/03_visualisations/map_pub_freq.Rds")
 
 
 
-reg_color <- c("#BF9983", "#FFFFFF", "#AF8DA5", "#54B195", "#DDDDDD")
+reg_color <- c("#BF9983", "#898989", "#AF8DA5", "#54B195", "#D8B04C")
+names(reg_color) <- unique(au_carto$region)[c(2,4,3,5,1)]
 
 r <- leaflet(au_carto) %>%
   # setView(-96, 37.8, 4) %>%
@@ -425,8 +206,9 @@ r <- r %>% addPolygons(
 rrr <- tags$div(
   HTML('<strong>United Nations geoscheme for Africa</strong>')
 )  
-map_cou_reg <- r %>% addLegend(colors = cou_col[1:6], labels=unique(au_carto$region), title="UNSD Regions", values = ~region_num, opacity = 0.7,
+map_cou_reg <- r %>% addLegend(colors = reg_color, labels=names(reg_color), title="AU Regions", values = ~region_num, opacity = 0.7,
                 position = "bottomright") %>%
-  addControl(rr, position = "bottomleft")
-?addLegend
+  addControl(rrr, position = "bottomleft")
 
+saveWidget(map_cou_reg, file="01_data/03_visualisations/map_cou_reg.html")
+saveRDS(map_cou_reg, file = "01_data/03_visualisations/map_cou_reg.Rds")
